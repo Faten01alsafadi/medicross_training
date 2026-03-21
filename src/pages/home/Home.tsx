e380c9af1241e2e4958aab8de2bb1e76e27424c9
faten <fatensafad@gmail.com>
2026-03-21 14:21:40 +0300

    first commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { MdInstallDesktop } from 'react-icons/md'
+
+
+function Home() {
+  return (
+    <div className='bg-blue-500'>
+      home
+      <MdInstallDesktop />
+     
+    </div>
+  )
+}
+
+export default Home
